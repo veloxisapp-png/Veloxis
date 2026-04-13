ddf1c4db0e6b90857533fa8c2c6daf041a0b5a0a
veloxisapp-png <veloxis.app@gmail.com>
2026-04-13 16:23:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToMarkets() {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+}
+
+function goBack() {
+  document.getElementById("page1").style.display = "block";
+  document.getElementById("page2").style.display = "none";
+}
